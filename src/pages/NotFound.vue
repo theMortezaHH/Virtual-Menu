a73fe160676c0b75e71618e87182e7fe6bad6e4a
theMortezaHH <mortezahh2022@gmail.com>
2023-09-10 20:12:00 +0330

    Not found page added

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+    <div class="container">
+        <p class="not-found">(404) صفحه مورد نظر یافت نشد</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100dvh;
+    width: 100dvw;
+    .not-found {
+        font-size: 5dvw;
+        direction: rtl;
+    }
+}
+</style>
