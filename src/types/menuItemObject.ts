558dbb9176d8883e33e6922b0566367294a780fc
theMortezaHH <mortezahh2022@gmail.com>
2023-11-16 09:29:25 +0330

    Creating new Vue + Primevue + Typescript project.

---
+++ src/types/menuItemObject.ts
@@ -0,0 +1,11 @@
+interface menuItemObject {
+    id: number
+    categoryId: number
+    image: string
+    title: string
+    duration: number
+    price: number
+    avalable: boolean
+}
+
+export default menuItemObject
